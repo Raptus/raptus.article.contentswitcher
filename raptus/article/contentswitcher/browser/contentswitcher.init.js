dd0a07497c7efa03422b30815c6124535dc8fa4b
Simon Käser <skaeser@raptus.com>
2011-03-08 12:04:47 +0000

    Prevent javascript from appending 'null' to the navigation if no manage links are available

--- raptus/article/contentswitcher/browser/contentswitcher.init.js
+++ raptus/article/contentswitcher/browser/contentswitcher.init.js
@@ -9,7 +9,7 @@
         var height = Math.min((params.settings.fixedHeight-(params.settings.matchedObjects.size()-1))/params.settings.matchedObjects.size());
       for(var i=0; i<params.settings.matchedObjects.size(); i++) {
         var obj = $(params.settings.matchedObjects.get(i));
-        var manage = obj.find('h2 > .manage').html();
+        var manage = obj.find('h2 > .manage').size() ? obj.find('h2 > .manage').html() : '';
         if(manage) manage = '<div class="manage">'+manage+'</span>';
         nav.append('<li class="o'+i+'"><a href="'+obj.find('h2 > a').attr('href')+'" title="'+obj.find('p').html()+'">'+obj.find('h2 > a').html()+'</a>'+manage+'</li>');
         nav.find('li:last').fadeTo(0, params.settings.contentswitcher.baseOpacity);
